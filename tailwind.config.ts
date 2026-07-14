08a64cdfdb3b0a7d25d59610f4a10fbf3e962c8f
Leonan Felipe <dev.leonan@gmail.com>
2026-07-14 11:40:03 -0300

    fix: move tailwind to main dependencies and update content paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,9 @@ import type { Config } from "tailwindcss";
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
